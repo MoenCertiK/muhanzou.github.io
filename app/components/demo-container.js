8e207e6651414b9d716eaa339525c12b7d72f492
MuhanZou <muhan0128@gmail.com>
2016-02-02 17:46:00 -0500

    add start page

--- app/components/demo-container.js
+++ app/components/demo-container.js
@@ -1,14 +1,29 @@
 import React, { Component, PropTypes } from 'react';
+import StartPage from './start-page';
+import HanoiTower from './demos/hanoi-tower';
 
 class DemoContainer extends Component {
   constructor(props) {
     super(props);
   }
 
+  renderDemo(id) {
+    let demo;
+    switch(id) {
+      case 0:
+        demo = <StartPage />;
+        break;
+      case 1:
+        demo = <HanoiTower />;
+        break;
+    }
+    return demo;
+  }
+
   render() {
     return (
       <div id="show-place">
-        {this.props.curSection.title}
+        {this.renderDemo(this.props.curSection.id)}
       </div>
     );
   }
---
+++ app/components/start-page.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react';
+
+class StartPage extends Component {
+  render() {
+    return (
+      <article>
+        <span className="title">Hummm</span>
+        <br /><br />
+        This is where I put my practice codes, and they were developed using <a href="https://facebook.github.io/react/" target="_blank">React.js</a>.<br />
+        Navigate through left to go over some shits I built...
+      </article>
+    );
+  }
+}
+
+StartPage.propTypes = {
+
+};
+
+export default StartPage;
